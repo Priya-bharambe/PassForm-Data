cfe85bc26116f56f633079dede87507ce6ea630c
Priya-bharambe <pryabharambe70@gmail.com>
2021-04-08 10:10:23 +0530

    first commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'passformdata'`, () => {
+  it(`should have as title 'PassFormdata'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('passformdata');
+    expect(app.title).toEqual('PassFormdata');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('passformdata app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('PassFormdata app is running!');
   });
 });
